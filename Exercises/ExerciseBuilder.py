7d496600be2c897644748904ade26084988c3115
DanGrutsyuk <Dan_Gritsyuk>
2023-04-15 15:42:54 +0300

    Update ExerciseBuillder class

--- Exercises/ExerciseBuilder.py
+++ Exercises/ExerciseBuilder.py
@@ -7,52 +7,42 @@ from Exercises.Homework2.Exercise6 import Exercise6
 from Exercises.Homework2.Exercise7 import Exercise7
 from Exercises.Homework2.Exercise8 import Exercise8
 from Exercises.ExerciseAbstract import ExerciseAbstract
+from Exercises.ExerciseData import ExerciseData
 
 
 class ExerciseBuilder:
     @staticmethod
     def GetExersice(taskNum) -> ExerciseAbstract:
+        taskText = ExerciseData().GetTaskText(taskNum)
         correctEnter = False
         while not correctEnter:
             match taskNum:
                 case 0:
                     return None
                 case 1:
-                    return Exercise1(
-                        'Задача 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и выводит название этого дня недели.'
-                    )
+                    return Exercise1(taskText)
                 case 2:
-                    return Exercise2(
-                        'Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.'
-                    )
+                    return Exercise2(taskText)
                 case 3:
-                    return Exercise3(
-                        'Задача 3. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).'
-                    )
+                    return Exercise3(taskText)
                 case 4:
-                    return Exercise4(
-                        'Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.'
-                    )
+                    return Exercise4(taskText)
                 case 5:
-                    return Exercise5(
-                        'Задача 1. Напишите программу, которая принимает на вход число N и выдает список факториалов для чисел от 1 до N.'
-                    )
+                    return Exercise5(taskText)
                 case 6:
-                    return Exercise6(
-                        'Задача 2. Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z.'
-                    )
+                    return Exercise6(taskText)
                 case 7:
-                    return Exercise7(
-                        'Задача 3. Даны две строки. Посчитайте сколько раз каждый символ первой строки встречается во второй'
-                    )
+                    return Exercise7(taskText)
                 case 7:
-                    return Exercise7(
-                        'Задача 3. Даны две строки. Посчитайте сколько раз каждый символ первой строки встречается во второй'
-                    )
+                    return Exercise7(taskText)
                 case 8:
-                    return Exercise8(
-                        'Задача 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Сдвиньте все элементы списка на 2 позиции вправо.'
-                    )
+                    return Exercise8(taskText)
+                case 9:
+                    return Exercise8(taskText)
+                case 10:
+                    return Exercise8(taskText)
+                case 11:
+                    return Exercise8(taskText)
                 case _:
                     print('Такой задачи нет. Повторите попытку...\n')
                     continue
